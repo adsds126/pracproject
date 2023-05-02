584b99b4aee7ab852e260cf32eee16129b937666
gimtaehyeong <adsds126@gmail.com>
2023-05-02 18:02:51 +0900

    Initial commit

---
+++ src/main/java/soloproject/pracproject/PracprojectApplication.java
@@ -0,0 +1,14 @@
+package soloproject.pracproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracprojectApplication.class, args);
+
+	}
+
+}
